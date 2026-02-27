aeb062fafdac2c9e36cd500d17b9a8a0c093e88f
ALICE <gandralice@gmail.com>
2026-02-27 10:55:10 -0300

    0501-01import

---
+++ 0501css-import/01import/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+const App = () => {
+  return (
+    <>
+      <div className="ativo">
+        <p>adsfaf</p>
+      </div>
+    </>
+  );
+};
+
+export default App;
