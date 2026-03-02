aca25a4b49dd2beaadd4a2540d262f6d6677fbd9
ALICE <gandralice@gmail.com>
2026-03-02 10:58:41 -0300

    0501componentes

---
+++ 0501css-import/02componentes/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Tilte from "./Tilte";
+// Ao importar um componentes, os estilos importados do mesmo são automaticamente adicionados ao CSS final da build. Independente de você utilizar o componente ou não.
+const App = () => {
+  return (
+    <>
+      <div className="ativo">
+        <p>adsfaf</p>
+      </div>
+    </>
+  );
+};
+
+export default App;
