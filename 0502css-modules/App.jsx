25c5c690705bcdddf9dd768fe7017b2002fdf257
ALICE <gandralice@gmail.com>
2026-03-05 11:48:47 -0300

    0502final

---
+++ 0502css-modules/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Produto from "./Components/Produto";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <Produto></Produto>
+      </div>
+    </>
+  );
+};
+
+export default App;
